3e63185c65dd868e800aba617b369d168b2ce99f
liangjunmeng <2394419129@qq.com>
2024-12-09 23:00:17 +0800

    包
    添加各类包

---
+++ src/com/xxxx/entity/User.java
@@ -0,0 +1,19 @@
+package com.xxxx.entity;
+
+public class User {
+    private Integer userId; //用户编号
+    private String userName; //用户名
+    private String userPwd; //用户密码
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+}
