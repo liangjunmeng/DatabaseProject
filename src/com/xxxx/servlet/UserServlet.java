8b92fbd5599d8532631cb93f757327c05163ab94
liangjunmeng <2394419129@qq.com>
2024-12-12 13:47:37 +0800

    小改
    改了一点点测试类输出

---
+++ src/com/xxxx/servlet/UserServlet.java
@@ -0,0 +1,21 @@
+package com.xxxx.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.IOException;
+/*
+@WebServlet("/login")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
+
+ */
